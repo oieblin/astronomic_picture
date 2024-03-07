9f4ff96a3d8d7abb4e5ce0f0907825368aa291fd
oieblin <vkubor.bk@gmail.com>
2024-03-07 06:22:09 +0300

    testcommit

---
+++ src/main/java/com/example/astronomic_picture/AstronomicPictureApplication.java
@@ -0,0 +1,13 @@
+package com.example.astronomic_picture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstronomicPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstronomicPictureApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/astronomic_picture/service/Service.java
@@ -0,0 +1,4 @@
+package com.example.astronomic_picture.service;
+
+public class Service {
+}
